2252f8899fdd931e7df134e0d364c70c76a9b82a
dr3zz <dr3zz@abv.bg>
2015-05-06 23:53:07 +0300

    add comment

--- views/questions/view.php
+++ views/questions/view.php
@@ -21,3 +21,30 @@
     </div>
     <div class="clearfix"></div>
 </div>
+<div class="login">
+    <h1>Leave Comment</h1>
+    <form action="/question/addComment" method="POST" class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label for="name" class="col-lg-4 control-label">name:</label>
+                <div class="col-lg-8">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="name...">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="email" class="col-lg-4 control-label">email:</label>
+                <div class="col-lg-8">
+                    <input type="email" name="email" class="form-control" id="email" placeholder="email">
+                </div>
+            </div>
+            <input type="hidden" name="id">
+            <input type="hidden" name="formToken" value="<?php echo $_SESSION['formToken'] ?>">
+            <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-6">
+                    <input type="submit" class="btn btn-primary" value="Login">
+                    <a href="/account/register" class="btn btn-default">Go Register</a>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</div>
