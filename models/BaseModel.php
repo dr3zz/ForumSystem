4a69b577d2ae328440777f8d04376d40eacabf64
dr3zz <dr3zz@abv.bg>
2015-05-08 19:50:14 +0300

    some fixes

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,11 +1,30 @@
+
+//abstract class BaseModel {
+//    protected static $db;
+//
+//    public function __construct() {
+//        if (self::$db == null) {
+//
+//            self::$db = new mysqli(
+//                DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//            self::$db->set_charset("utf8");
+//            if (self::$db->connect_errno) {
+//                die('Cannot connect to database');
+//            }
+//        }
+//    }
+//}
+
 <?php
 abstract class BaseModel {
     protected static $db;
 
     public function __construct() {
         if (self::$db == null) {
+            $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+//            self::$db = new PDO($dsn, DB_USER, DB_PASS);
             self::$db = new mysqli(
-                DB_HOST, DB_USER, DB_PASS, DB_NAME);
+                $dsn, DB_USER, DB_PASS);
             self::$db->set_charset("utf8");
             if (self::$db->connect_errno) {
                 die('Cannot connect to database');
