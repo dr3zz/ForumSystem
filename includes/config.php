47bc07720b5d526954211afa3bc4d3a894552b3c
dr3zz <dr3zz@abv.bg>
2015-05-08 19:38:45 +0300

    some fixes

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,13 @@ define('DEFAULT_CONTROLLER', 'home');
 define('DEFAULT_ACTION', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', 'localhost');
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'forum');
+//define('DB_PASS', '1234');
+//define('DB_NAME', 'forum_system');
+//define('DEFAULT_PAGE_SIZE', 4);
+
+define('DB_HOST', 'softuni_forum');
 define('DB_USER', 'forum');
 define('DB_PASS', '1234');
 define('DB_NAME', 'forum_system');
